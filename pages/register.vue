21ee54df9bcc2fbb1e7594494b185aa00adf666d
Paulo Cardoso <paulo@agily.com.b>
2021-03-14 18:11:10 -0300

    register done

--- pages/register.vue
+++ pages/register.vue
@@ -1,10 +1,59 @@
 <template>
   <div class="container">
-    <h1>Novo Cadastro</h1>
+    <div class="card mt-3">
+      <div class="card-header">Registrar</div>
+      <div class="card-body">
+        <form action="#">
+          <p class="card-text">Faça seu cadastro</p>
+
+          <input v-model="name" type="text" class="form-control" />
+          <input v-model="email" type="text" class="form-control" />
+          <input v-model="password" type="password" class="form-control" />
+
+          <span class="btn btn-primary mt-3" @click="register">Cadastrar</span>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   auth: false,
+  data() {
+    return {
+      name: 'teste',
+      email: 'teste@teste.com',
+      password: 'admin',
+    }
+  },
+  mounted() {},
+  methods: {
+    async register() {
+      try {
+        await this.$axios.post('/auth/register', {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        })
+
+        const data = {
+          email: this.email,
+          password: this.password,
+        }
+
+        await this.$auth.loginWith('local', {
+          data,
+        })
+
+        location.reload()
+      } catch (err) {
+        if (err.response.data.email[0]) {
+          alert(err.response.data.email[0])
+        }
+
+        console.log(err.response.data)
+      }
+    },
+  },
 }
 </script>
